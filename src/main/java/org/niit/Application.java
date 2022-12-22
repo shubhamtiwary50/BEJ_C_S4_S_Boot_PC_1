18223e5db072c780fff477924945dbcafe420527
SHUBHAM <shubhamtiwary50@gmail.com>
2022-12-22 18:02:54 +0530

    Initial commit

---
+++ src/main/java/org/niit/Application.java
@@ -0,0 +1,15 @@
+package org.niit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+		System.out.println("------------||----------||-----------");
+		System.out.println("Good Day to You all !");
+	}
+
+}
